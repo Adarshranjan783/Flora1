1215eb5e9e40ae01cb21a55ffe828b16ad0f20a2
Adarsh Ranjan <88224901+Adarshranjan783@users.noreply.github.com>
2022-02-25 00:58:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -67,66 +67,103 @@ def select():
     return render_template('retrieve.html', floralist=floralist)
 
 
+@app.route('/retrieve/color/<string:color>')
+def retereve1(color):
+
+    c=Flora.query.filter_by(color=color).count()
+    if c > 0:
+
+        #flower = Flora.query.filter_by(color=color).all()
+        #return render_template('result.html', flower=flower)
+            
+        user_schema = UserSchema()
+        user_schema = UserSchema(many=True)
+        users = Flora.query.filter_by(color=color).all()
+        output = user_schema.dump(users)
+        print(output)
+        return jsonify({'flower' : output})
+        
+    else:
+        #floralist = Flora.query.all() 
+        #return render_template('noresult.html', floralist=floralist)
+        user_schema = UserSchema()
+        user_schema = UserSchema(many=True)
+        users = Flora.query.all() 
+        output = user_schema.dump(users)
+        print(output)
+        return jsonify({'flower' : output})
+
 @app.route('/retrieve/color', methods=['GET', 'POST'])
-@cache.cached(timeout=50)
+@cache.cached(timeout=5)
 def retereve():
     if request.method=='POST':
         color = request.form['color']
-        c=Flora.query.filter_by(color=color).count()
-        if c > 0:
+        return redirect(f"/retrieve/color/{color}")
+        
 
-            #flower = Flora.query.filter_by(color=color).all()
-            #return render_template('result.html', flower=flower)
+@app.route('/retrieve/type/<string:type>')
+def reterevetype2(type):
+
+    c=Flora.query.filter_by(type=type).count()
+    if c > 0:
+
+        #flower = Flora.query.filter_by(color=color).all()
+        #return render_template('result.html', flower=flower)
             
-            user_schema = UserSchema()
-            user_schema = UserSchema(many=True)
-            users = Flora.query.filter_by(color=color).all()
-            output = user_schema.dump(users)
-            print(output)
-            return jsonify({'flower' : output})
+        user_schema = UserSchema()
+        user_schema = UserSchema(many=True)
+        users = Flora.query.filter_by(type=type).all()
+        output = user_schema.dump(users)
+        print(output)
+        return jsonify({'flower' : output})
         
-        else:
-            #floralist = Flora.query.all() 
-            #return render_template('noresult.html', floralist=floralist)
-            user_schema = UserSchema()
-            user_schema = UserSchema(many=True)
-            users = Flora.query.all() 
-            output = user_schema.dump(users)
-            print(output)
-            return jsonify({'flower' : output})
-        
-    #flower = Flora.query.filter_by(sno=sno).first()
-    #return render_template('result.html', flower=flower)
+    else:
+        #floralist = Flora.query.all() 
+        #return render_template('noresult.html', floralist=floralist)
+        user_schema = UserSchema()
+        user_schema = UserSchema(many=True)
+        users = Flora.query.all() 
+        output = user_schema.dump(users)
+        print(output)
+        return jsonify({'flower' : output})
 
 @app.route('/retrieve/type', methods=['GET', 'POST'])
 @cache.cached(timeout=5)
 def reterevetype():
     if request.method=='POST':
         type = request.form['type']
-        f=Flora.query.filter_by(type=type).count()
+        return redirect(f"/retrieve/type/{type}")
 
-        if f > 0:
-            #flower = Flora.query.filter_by(type=type).all()
-            #return render_template('result.html', flower=flower)
-            user_schema = UserSchema()
-            user_schema = UserSchema(many=True)
-            users = Flora.query.filter_by(type=type).all()
-            output = user_schema.dump(users)
-            print(output)
-            return jsonify({'flower' : output})
-        
-        else:
-            #floralist = Flora.query.all() 
-            #return render_template('noresult.html', floralist=floralist)
-            user_schema = UserSchema()
-            user_schema = UserSchema(many=True)
-            users = Flora.query.all() 
-            output = user_schema.dump(users)
-            print(output)
-            return jsonify({'flower' : output})
-        
-    #flower = Flora.query.filter_by(sno=sno).first()
-    #return render_template('result.html', flower=flower)
+
+@app.route('/retrieve/price/<int:min>/<int:max>')
+def reterevepric2(min,max):
+
+    c=Flora.query.filter(Flora.price <= max).\
+            filter(Flora.price >= min).count()
+    m = Flora.query.filter(Flora.price <= max).\
+            filter(Flora.price >= min).count()
+    if m > 0:
+        #todo =Flora.query.filter(Flora.price <= max).\
+        #filter(Flora.price >= min).all()
+        #return render_template('result.html', todo=todo)
+        user_schema = UserSchema()
+        user_schema = UserSchema(many=True)
+        users = Flora.query.filter(Flora.price <= max).\
+        filter(Flora.price >= min).all()
+        output = user_schema.dump(users)
+        print(output)
+        return jsonify({'flower' : output})
+
+
+    else:
+        #floralist = Flora.query.all() 
+        #return render_template('noresult.html', floralist=floralist)
+        user_schema = UserSchema()
+        user_schema = UserSchema(many=True)
+        users = Flora.query.all() 
+        output = user_schema.dump(users)
+        print(output)
+        return jsonify({'flower' : output})
 
 @app.route('/retrieve/price', methods=['GET', 'POST'])
 @cache.cached(timeout=5)
@@ -134,34 +171,7 @@ def retereveprice():
     if request.method=='POST':
         min = request.form['min']
         max = request.form['max']
-        m = Flora.query.filter(Flora.price <= max).\
-            filter(Flora.price >= min).count()
-        if m > 0:
-            #flower =Flora.query.filter(Flora.price <= max).\
-            #filter(Flora.price >= min).all()
-            #return render_template('result.html', flower=flower)
-            user_schema = UserSchema()
-            user_schema = UserSchema(many=True)
-            users = Flora.query.filter(Flora.price <= max).\
-            filter(Flora.price >= min).all()
-            output = user_schema.dump(users)
-            print(output)
-            return jsonify({'flower' : output})
-
-
-        else:
-            #floralist = Flora.query.all() 
-            #return render_template('noresult.html', floralist=floralist)
-            user_schema = UserSchema()
-            user_schema = UserSchema(many=True)
-            users = Flora.query.all() 
-            output = user_schema.dump(users)
-            print(output)
-            return jsonify({'flower' : output})
-
-        
-    #flower = Flora.query.filter_by(sno=sno).first()
-    #return render_template('result.html', flower=flower)
+        return redirect(f"/retrieve/price/{min}/{max}")
 
 @app.route('/update/<int:sno>', methods=['GET', 'POST'])
 def update(sno):
